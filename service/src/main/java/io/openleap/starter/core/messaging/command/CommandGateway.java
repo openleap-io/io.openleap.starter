f81127915bb5027f8174538317b4e777eb044374
Dr. Sören Kemmann <soeren.kemmann@bb-online.de>
2025-12-04 13:04:10 +0100

    added command to messageing

---
+++ service/src/main/java/io/openleap/starter/core/messaging/command/CommandGateway.java
@@ -0,0 +1,28 @@
+/*
+ * This file is part of the openleap.io software project.
+ *
+ *  Copyright (C) 2025 Dr.-Ing. Sören Kemmann
+ *
+ * This software is dual-licensed under:
+ *
+ * 1. The European Union Public License v.1.2 (EUPL)
+ *    https://joinup.ec.europa.eu/collection/eupl
+ *
+ *     You may use, modify and redistribute this file under the terms of the EUPL.
+ *
+ *  2. A commercial license available from:
+ *
+ *     B+B Unternehmensberatung GmbH & Co.KG
+ *     Robert-Bunsen-Straße 10
+ *     67098 Bad Dürkheim
+ *     Germany
+ *     Contact: license@bb-online.de
+ *
+ *  You may choose which license to apply.
+ */
+package io.openleap.starter.core.messaging.command;
+
+/** Gateway for dispatching commands to their handlers. */
+public interface CommandGateway {
+    <R> R send(Command command);
+}
